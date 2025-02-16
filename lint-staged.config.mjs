5b834e8561dabd8a490474eff1dfb1c07884ba69
Philipp Lentzen <code@philipplentzen.dev>
2025-02-16 16:54:51 +0100

    chore: initialize lint-staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('lint-staged').Configuration}
+ */
+const lintStagedConfig ={
+  "./src/**/*.{ts,js,jsx,tsx}": [
+    "eslint --fix",
+    "prettier . --write"
+  ]
+};
+
+export default lintStagedConfig;
