a57ece615273b84e993a74132989aff6109cee1b
Philipp Lentzen <code@philipplentzen.dev>
2025-02-16 17:01:03 +0100

    chore: update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,11 +2,35 @@
  * @type {import("prettier").Config}
  */
 const prettierConfig = {
-  printWidth: 120,
-  semi: true,
-  singleQuote: false,
+  experimentalTernaries: true,
+  printWidth: 80,
   tabWidth: 2,
-  trailingComma: "es5",
+  useTabs: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  bracketSameLine: false,
+  arrowParens: "always",
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-sort-json",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/(.*)$",
+    "^[./]",
+    "",
+    "<TYPES>",
+    "<TYPES>^[./]",
+  ],
+  tailwindStylesheet: "./app/globals.css",
 };
 
 export default prettierConfig;
