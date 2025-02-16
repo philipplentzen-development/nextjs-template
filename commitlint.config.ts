06b21b0746bf0eff0ebe0552ef5f134fa9467793
Philipp Lentzen <code@philipplentzen.dev>
2025-02-16 18:58:16 +0100

    chore: update commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,26 @@
-const commitlintConfig = {
+import { RuleConfigSeverity, UserConfig } from "@commitlint/types";
+
+const commitlintConfig: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "release",
+      ],
+    ],
+  },
 };
 
 export default commitlintConfig;
