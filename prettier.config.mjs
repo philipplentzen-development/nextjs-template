2fef07872cf608c523e514d300ea702f0c159bab
Philipp Lentzen <code@philipplentzen.dev>
2025-02-16 16:40:26 +0100

    chore: initialize prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const prettierConfig = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
+
+export default prettierConfig;
